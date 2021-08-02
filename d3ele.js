0e361f529ca775ecab27ad5a6ded192a1deb8b87
lotusonion <sicongh3@illinois.edu>
2021-08-02 14:56:23 -0500

    adj9

--- d3ele.js
+++ d3ele.js
@@ -212,7 +212,7 @@ async function load2() {
                     tooltip.transition().duration(200)
                         .style('opacity', 0.9)
                         .style('left', (event.x ) + 'px')
-                        .style('top', 2300+'px')
+                        .style('top', '200px')
                     tooltip.html(d.Make+"<br>"+d.AverageCityMPG)
             })
                 .on("mouseout", function (d) {

aa5b75d8c0e5687c0ea3489fd3352f83c819e645
lotusonion <sicongh3@illinois.edu>
2021-08-01 23:06:29 -0500

    change vars

---
+++ d3ele.js
@@ -0,0 +1,126 @@
+var scene1 = d3.select('#scene1')
+            var scene2 = d3.select('#scene2')
+            var scene3 = d3.select('#scene3')
+            var width = 950, height = 950, spacing=120;
+            var margin = { top: 10, right: 100, bottom: 50, left: 50 },
+                width = 1000 - margin.left - margin.right,
+                height = 1000 - margin.top - margin.bottom;
+
+
+            const data =d3.csv("narrativevisualization/cars2017.csv");
+            var makes = ["Acura", "Alfa Romeo", "Audi", "BMW", "Buick",  "Cadillac", "Chevrolet", "Chrysler",
+                "Dodge", "Fiat", "Ford", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
+                "Land Rover", "Lexus", "Lincoln","Mazda", , "Mercedes-Benz", "MINI", "Mitsubishi",
+                "Nissan", "Porsche", "Ram", "Subaru", "Toyota", "Volkswagen", "Volvo"];
+
+            var highway_mpgs = ["35", "33", "32", "32", "31", "32", "34", "36", "25", "34", "31", "27", "38", "34", "27", "39", "31", "33",
+                "27", "36", "29", "35", "30", "33", "31", "33", "27", "28", "33", "35", "33", "29"];
+
+            var city_mpgs = ["25", "24", "24", "23", "23", 
+            "22", "27", "23", "19", "26", "24", "20", "30", "27", "25", "30", "24", "27", 
+                "20", "39", "23", "27", "23", "24", "24", "26", "21", "21", "25", "32", "24", "23"];
+
+
+
+            var y=d3.scaleLinear().domain([0, 120]).range([height-spacing,0]);
+            var x=d3.scaleLinear().domain([10, 20, 30, 40]).range([1,width-spacing]);
+
+            var xAxis = d3.axisBottom()
+                .scale(x)
+
+            var yAxis = d3.axisLeft()
+                .scale(y)
+
+            
+            scene1.append("g").append("g").call(d3.axisLeft(y));
+            scene1.append("g").attr("transform","translate(0,"+(height-spacing) +")").call(d3.axisBottom(x));
+            scene1.attr("class", "center-screen");
+            scene1.append('text')
+                .attr('x', -500)
+                .attr('y', 15)
+                .attr('transform', 'rotate(-90)')
+                .attr('text-anchor', 'middle')
+                .text('Mileage')
+
+            scene1.append('text')
+                .attr('x', 500)
+                .attr('y', 1050)
+                .attr('text-anchor', 'middle')
+                .text('Car Brand')
+            scene2.append('text')
+                .attr('x', 500)
+                .attr('y', 390)
+                .attr('text-anchor', 'middle')
+                .text('Average Highway MPG')
+
+            var bar_tooltip = d3.select("body")
+                .append("div")
+                .append("div")
+                .style("opacity", 0)
+                .attr("class", "tooltip")
+                .style("background-color", "black")
+                .style("border-radius", "5px")
+                .style("padding", "15px")
+                .style("color", "white")
+
+
+
+        async function load1() {
+                var makeScale = d3.scaleBand()
+                    .range([0, width])
+                    .domain(data_given.map(function (d) { return d.Make; }))
+
+                var makeAxis = d3.axisBottom()
+                    .scale(makeScale)
+                    .ticks(5);
+
+                scene1.append("g")
+                    .attr("transform", "translate(50,950)")
+                    .attr("class", "axis")
+                    .call(makeAxis)
+                    .selectAll("text")
+                    .attr("transform", "translate(-10,0)rotate(-30)")
+                    .style("text-anchor", "end");
+
+                scene1.selectAll("mybar")
+                    .data(data_given)
+                    .enter()
+                    .append("rect")
+                    .attr("x", function (d, i) { return margin.left + makeScale(makes[i]); })
+                    .attr("y", function (d, i) { return y(highway_mpgs[i]) + 10; })
+                    .attr("width", makeScale.bandwidth() - 10)
+                    .attr("height", function (d, i) { return height - y(highway_mpgs[i]); })
+                    .attr("fill", "#5E4FA2").on("mouseover", function (d, i) {
+                        bar_tooltip.transition()
+                            .duration(200)
+                            .style("opacity", .9);
+                        bar_tooltip.html(makes[i])
+                            .style("left", (d3.event.pageX) + "px")
+                            .style("top", (d3.event.pageY - 28) + "px");
+                    })
+                    .on("mouseout", function (d) {
+                        bar_tooltip.transition()
+                            .duration(500)
+                            .style("opacity", 0);
+                    });
+            })
+        }
+
+// This function is called by the buttons on top of the plot
+function change(setting) {
+    if (setting === "AverageHighwayMPG") {
+        scene1.selectAll("rect")
+            .transition()
+            .duration(2000)
+            .attr("fill", "#5E4FA2")
+            .attr("y", function (d, i) { return y(highway_mpgs[i]) + 10; })
+            .attr("height", function (d, i) { return height - y(highway_mpgs[i]); })
+    } else {
+        scene1.selectAll("rect")
+            .transition()
+            .duration(2000)
+            .attr("fill", "#66C2A5")
+            .attr("y", function (d, i) { return y(city_mpgs[i]) + 10; })
+            .attr("height", function (d, i) { return height - y(city_mpgs[i]); })
+    }
+}
